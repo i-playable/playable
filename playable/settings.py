7ac3039b00f323144f1d5bcf410d3a9d9dfcb951
Kowit C <kowit@i-playable.com>
2015-08-13 14:21:35 +0700

    Add templates

--- playable/settings.py
+++ playable/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'playable.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,4 +104,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
--- playable/urls.py
+++ playable/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+import settings
+
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
++ staticfiles_urlpatterns()
