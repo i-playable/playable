c283ee1560eb8e5d6aa90e500efe101b40ebfc91
Kowit C <kowit@i-playable.com>
2015-09-18 18:14:54 +0700

    Add Products Descriptions

--- product/models.py
+++ product/models.py
@@ -38,6 +38,7 @@ class Product(models.Model):
     code = models.CharField(max_length=250, unique=True)
     price = models.IntegerField(null=True, blank=True)
     amout = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __unicode__(self):
         return self.name
